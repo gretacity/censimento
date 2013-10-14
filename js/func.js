41146bbab4db130c32871190bab085d35fe58f0c
Gretacity Developer <info@gretacity.com>
2013-10-14 11:36:20 +0200

    risoluzione fastidi comuni

--- js/func.js
+++ js/func.js
@@ -214,16 +214,22 @@ function initApplication(){
 	
 	$("#inizia_censimento").tap(function(){
 		
+		// CONSOLE LOG
 		console.log("aggiorna e azzera i valori");		
 	    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-	    
+	   
+	    // CAMBIA PAGINA (PRINCIPALE)
 	    $.mobile.changePage("#roadpage");	
 	    
+	    // SVUOTA I CAMPI IMPORTANTI 
 	    $("#latitude").val("");
 		$("#longitude").val("");
 		$("#accuracy").val("");
 		
+		// IMPOSTA IL COLORE ROSSO SU I TEXTBOX IMPORTANTI
 		$("#latitude, #longitude,#accuracy").css("background-color","#f2dede");
+		
+		
 
 	});
 }
@@ -335,15 +341,18 @@ function dettagli_staffe(indice){
 
 // SELEZIONA CARTELLO DA LISTA DI RICERCA
 function seleziona_cartello(index,value){
-
+	
 	// RIMUOVI GLI ALTRI RISULTATI DELLA RICERCA		
 	$('#suggestions_'+index+' li[id!='+value+']').remove();
 	
 	// INSERISCI DATO IN CAMPO NASCOSTO
 	$('#cartello_segnale_'+index).val(value);
 	
+	// CAMBIO COLORE DI SFONDO ALL'ELEMENTO DELLA LISTA
+	$('#suggestions_'+index+' li[id='+value+']').attr("data-theme", "b").removeClass("ui-btn-up-c").removeClass('ui-btn-hover-c').addClass("ui-btn-up-selezionato").addClass("ui-btn-hover-selezionato");
+
 	// SCRIVI CARTELLO SELEZIONATO
-	$('#cartello_selezionato_messaggio_'+index).html("<i>Cartello Selezionato</i>").css("color","green");
+	//$('#cartello_selezionato_messaggio_'+index).html("<i>Cartello Selezionato</i>").css("color","green");
 
 }
 
@@ -359,74 +368,79 @@ function cambia_tipologia(select_tipologia_name,index){
 function dettaglio_cartelli(numero_cartelli){
 		
 	var str = "";
-		
-	for(var i=0;i<numero_cartelli;i++){
 	
-		str = "<h3>Cartello "+(i+1)+"</h3>"+
-			  "<p>"+				  
-			  "<label>Inserisci Figura C.d.S. o breve Descrizione:</label>"+		
-			  " <p>"+
-						"<input type='search' id='cerca_segnale_"+i+"' name='cerca_segnale_"+i+"' placeholder='Cerca Segnale'/>"+
-						"<a onclick='cerca_cartello("+i+")' data-role='button'>Cerca</a>"+ 
-						"<input type='hidden' id='cartello_segnale_"+i+"' name='cartello_segnale_"+i+"' />"+
-						"<ul id='suggestions_"+i+"' data-role='listview' data-inset='true' data-split-theme='d'></ul>"+
-			  " </p>"+
-			  "</p>"+
-		      "<p>"+
-	          "   <label>Supporto:</label>"+
-			  "	    <select name='cartello_supporto_"+i+"'>"+
-			  "        <option value='1' selected>Ferro</option>"+
-			  "        <option value='2'>Alluminio</option>"+
-			  "     </select>"+
-	          "</p>"+
-        	  "<p>"+
-              "   <label>Pellicola:</label>"+
-			  "     <select name='cartello_pellicola_"+i+"'>"+
-			  "        <option value='1' selected>E.G. Classe I</option>"+
-			  "        <option value='2'>H.I. Classe II</option>"+
-			  "        <option value='3'>H.I. Special III</option>"+
-			  "     </select>"+
-			  "</p>"+
-			  "<hr />"+
-			  "<p>"+
-	          "   <label>Tipo Intervento:</label>"+
-			  "      <select onchange='dettaglio_intervento_descrizione("+i+")' id='cartello_necessario_intervento_tipo_"+i+"' name='cartello_necessario_intervento_tipo_"+i+"'>"+
-			  "        <option value='0' selected>Non Necessario</option>"+
-			  "        <option value='1'>Sostituzione Parziale</option>"+
-			  "        <option value='2'>Sostituzione Totale</option>"+
-  			  "        <option value='3'>Soggetto a Manutenzione</option>"+
-			  "      </select>"+
-			  "</p>"+
-			  "<p id='div_necessario_intervento_descrizione_"+i+"' style='display:none;'>"+
-	          "   <label>Descrizione dell'intervento:</label>"+
-			  "	    <textarea id='cartello_necessario_intervento_descrizione_"+i+"' name='cartello_necessario_intervento_descrizione_"+i+"'></textarea>"+
-	          "</p>"+
-			  "<hr />"+
-			  "<p>"+
-	          "   <label>Forma:</label>"+
-			  "      <select onchange='dettaglio_forma("+i+")' id='cartello_forma_"+i+"' name='cartello_forma_"+i+"'>"+
-			  "        <option value='0' selected>Quadrato</option>"+
-			  "        <option value='1'>Rombo</option>"+
-			  "        <option value='2'>Rettangolo</option>"+
-  			  "        <option value='3'>Triangolo</option>"+
-			  "        <option value='4'>Cerchio</option>"+
-  			  "        <option value='5'>Ottagono</option>"+
-			  "      </select>"+
-			  "</p>"+
-			  "<p >"+
-	          "   <label>Lato (cm):</label>"+
-			  "      <input type='text' id='cartello_lato_"+i+"' name='cartello_lato_"+i+"' placeholder='Dimensione (cm)'/>"+
-	          "</p>"+
-	          "<p id='div_altezza_"+i+"' style='display:none;' >"+
-	          "   <label>Altezza (cm):</label>"+
-			  "      <input type='text' id='cartello_altezza_"+i+"' name='cartello_altezza_"+i+"' placeholder='Dimensione (cm)'/>"+
-	          "</p>"+
-	          "<hr />";
+	// IMPLEMENTAZIONE DELLA CONFERMA
+	if(numero_cartelli >= 10 && !confirm("Sei sicuro di voler inserire tutti questi Cartelli?"))
+		return;
+	
+	else{
+		
+		for(var i=0;i<numero_cartelli;i++){
+	
+			str = "<h3>Cartello "+(i+1)+"</h3>"+
+				  "<p>"+				  
+				  "<label>Inserisci Figura C.d.S. o breve Descrizione:</label>"+		
+				  " <p>"+
+							"<input type='search' id='cerca_segnale_"+i+"' name='cerca_segnale_"+i+"' placeholder='Cerca Segnale'/>"+
+							"<a onclick='cerca_cartello("+i+")' data-role='button'>Cerca</a>"+ 
+							"<input type='hidden' id='cartello_segnale_"+i+"' name='cartello_segnale_"+i+"' />"+
+							"<ul id='suggestions_"+i+"' data-role='listview' data-inset='true' data-split-theme='d'></ul>"+
+				  " </p>"+
+				  "</p>"+
+			      "<p>"+
+		          "   <label>Supporto:</label>"+
+				  "	    <select name='cartello_supporto_"+i+"'>"+
+				  "        <option value='1' selected>Ferro</option>"+
+				  "        <option value='2'>Alluminio</option>"+
+				  "     </select>"+
+		          "</p>"+
+	        	  "<p>"+
+	              "   <label>Pellicola:</label>"+
+				  "     <select name='cartello_pellicola_"+i+"'>"+
+				  "        <option value='1' selected>E.G. Classe I</option>"+
+				  "        <option value='2'>H.I. Classe II</option>"+
+				  "        <option value='3'>H.I. Special III</option>"+
+				  "     </select>"+
+				  "</p>"+
+				  "<hr />"+
+				  "<p>"+
+		          "   <label>Tipo Intervento:</label>"+
+				  "      <select onchange='dettaglio_intervento_descrizione("+i+")' id='cartello_necessario_intervento_tipo_"+i+"' name='cartello_necessario_intervento_tipo_"+i+"'>"+
+				  "        <option value='0' selected>Non Necessario</option>"+
+				  "        <option value='1'>Sostituzione Parziale</option>"+
+				  "        <option value='2'>Sostituzione Totale</option>"+
+	  			  "        <option value='3'>Soggetto a Manutenzione</option>"+
+				  "      </select>"+
+				  "</p>"+
+				  "<p id='div_necessario_intervento_descrizione_"+i+"' style='display:none;'>"+
+		          "   <label>Descrizione dell'intervento:</label>"+
+				  "	    <textarea id='cartello_necessario_intervento_descrizione_"+i+"' name='cartello_necessario_intervento_descrizione_"+i+"'></textarea>"+
+		          "</p>"+
+				  "<hr />"+
+				  "<p>"+
+		          "   <label>Forma:</label>"+
+				  "      <select onchange='dettaglio_forma("+i+")' id='cartello_forma_"+i+"' name='cartello_forma_"+i+"'>"+
+				  "        <option value='0' selected>Quadrato</option>"+
+				  "        <option value='1'>Rombo</option>"+
+				  "        <option value='2'>Rettangolo</option>"+
+	  			  "        <option value='3'>Triangolo</option>"+
+				  "        <option value='4'>Cerchio</option>"+
+	  			  "        <option value='5'>Ottagono</option>"+
+				  "      </select>"+
+				  "</p>"+
+				  "<p >"+
+		          "   <label>Lato (cm):</label>"+
+				  "      <input type='text' id='cartello_lato_"+i+"' name='cartello_lato_"+i+"' placeholder='Dimensione (cm)'/>"+
+		          "</p>"+
+		          "<p id='div_altezza_"+i+"' style='display:none;' >"+
+		          "   <label>Altezza (cm):</label>"+
+				  "      <input type='text' id='cartello_altezza_"+i+"' name='cartello_altezza_"+i+"' placeholder='Dimensione (cm)'/>"+
+		          "</p>"+
+		          "<hr />";
 			  
 			 
-			 $('#dettaglio_cartelli').append(str);
-
-
+				 $('#dettaglio_cartelli').append(str);
+			}	
 	}
 	
 	$('#dettaglio_cartelli').trigger('create');
@@ -486,33 +500,40 @@ function dettaglio_omologazione(numero_cartelli){
 function dettaglio_pali(numero_pali){
 	
 	var str = "";
+	
+	// IMPLEMENTAZIONE DELLA CONFERMA
+	if(numero_pali >= 10 && !confirm("Sei sicuro di voler inserire tutti questi Pali?"))
+		return;
+	
+	else{
 		
-	for(var i=0;i<numero_pali;i++){
-	
-		str = "<h3>Palo "+(i+1)+"</h3>"+
-			  "<p>"+
-            	 "<label>Dimensione:</label>"+
-				 "<input id='palo_dimensione_"+i+"' type='number' name='palo_dimensione_"+i+"' title='Formato 1.20' placeholder='Formato 1.20' />"+
-			  "</p>"+
-        	  "<p>"+
-    			 "<div data-role='fieldcontain' >"+					 
-					"<fieldset data-role='controlgroup'>"+
-						"<input type='checkbox' name='palo_controvento_"+i+"' id='palo_controvento_"+i+"' onclick='dettagli_palo_controvento("+i+")' class='custom' />"+
-						"<label for='palo_controvento_"+i+"'>Palo Controvento</label>"+
-				    "</fieldset>"+
-				 "</div>"+
-			     "<!-- se il cartello è omologato -->"+
-			     "<div id='palo_controvento_dettagli_"+i+"' style='display:none'>"+
-			    	"<p>"+
-			    		"<label>Dimensione Palo Controvento:</label><br />"+
-			    		"<input id='palo_controvento_dimensione_"+i+"' type='number' name='palo_controvento_dimensione_"+i+"' placeholder='Formato 1.20'/>"+
-			    	"</p>"+
-			   	 "</div>"+
-        	  "</p>"+
-        	  "<hr />";
-	
-		$("#dettaglio_pali").append(str);
-	
+		for(var i=0;i<numero_pali;i++){
+		
+			str = "<h3>Palo "+(i+1)+"</h3>"+
+				  "<p>"+
+	            	 "<label>Dimensione:</label>"+
+					 "<input id='palo_dimensione_"+i+"' type='number' name='palo_dimensione_"+i+"' title='Formato 1.20' placeholder='Formato 1.20' />"+
+				  "</p>"+
+	        	  "<p>"+
+	    			 "<div data-role='fieldcontain' >"+					 
+						"<fieldset data-role='controlgroup'>"+
+							"<input type='checkbox' name='palo_controvento_"+i+"' id='palo_controvento_"+i+"' onclick='dettagli_palo_controvento("+i+")' class='custom' />"+
+							"<label for='palo_controvento_"+i+"'>Palo Controvento</label>"+
+					    "</fieldset>"+
+					 "</div>"+
+				     "<!-- se il cartello è omologato -->"+
+				     "<div id='palo_controvento_dettagli_"+i+"' style='display:none'>"+
+				    	"<p>"+
+				    		"<label>Dimensione Palo Controvento:</label><br />"+
+				    		"<input id='palo_controvento_dimensione_"+i+"' type='number' name='palo_controvento_dimensione_"+i+"' placeholder='Formato 1.20'/>"+
+				    	"</p>"+
+				   	 "</div>"+
+	        	  "</p>"+
+	        	  "<hr />";
+		
+			$("#dettaglio_pali").append(str);
+		
+		}
 	}
 	
 	$('#dettaglio_pali').trigger('create');
@@ -522,20 +543,28 @@ function dettaglio_staffe(numero_staffe){
 	
 	var str = "";
 	
-	for(var i=0;i<numero_staffe;i++){
+	// IMPLEMENTAZIONE DELLA CONFERMA
+	if(numero_staffe >= 10 && !confirm("Sei sicuro di voler inserire tutti queste Staffe?"))
+		return;
 	
-		str = "<h3>Staffa "+(i+1)+"</h3>"+
-			  "<p>"+
-        	  "<select name='staffa_"+i+"'>"+
-        	  "  <option value='1' selected>Monofacciale</option>"+
-        	  "  <option value='2'>Bifacciale</option>"+
-        	  "</select>"+
-        	  "</p>";
-       
-        $("#dettaglio_staffe").append(str);
-     
-    }
-    
+	else{
+		
+		for(var i=0;i<numero_staffe;i++){
+		
+			str = "<h3>Staffa "+(i+1)+"</h3>"+
+				  "<p>"+
+	        	  "<select name='staffa_"+i+"'>"+
+	        	  "  <option value='1' selected>Monofacciale</option>"+
+	        	  "  <option value='2'>Bifacciale</option>"+
+	        	  "</select>"+
+	        	  "</p>";
+	       
+	        $("#dettaglio_staffe").append(str);
+	     
+	    }
+   	
+   	}
+	
 	$('#dettaglio_staffe').trigger('create');
 	
 }
@@ -579,13 +608,13 @@ function inizializza_road(){
 	
 	// azzero i campi principali
 	$("#qr_code").val("");	
-	//$("#latitude").val("");
-	//$("#longitude").val("");
-	//$("#accuracy").val("");
+
+	// cancello le foto
 	$("#foto_fronte").val("");
 	$("#foto_retro").val("");
 	$("#foto_prospettiva").val("");
 	
+	// elimino i tag di foto selezionate
 	$("#tag_foto_fronte").remove();
 	$("#tag_foto_retro").remove();
 	$("#tag_foto_prospettiva").remove();
