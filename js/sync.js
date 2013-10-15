89524f4769f240eca3fe699701e81f438141b429
Gretacity Developer <info@gretacity.com>
2013-10-15 10:44:04 +0200

    risolto problema se manca la connessione

--- js/sync.js
+++ js/sync.js
@@ -1,34 +1,45 @@
 function sync_data(){
 	
-	// DISABILITA PULSANTE  
-	$("#sync_data").addClass('ui-disabled');   
+	if(confirm("Questa procedura potrebbe impiegare del tempo, vuoi continuare?")){
+	
+		// DISABILITA PULSANTE  
+		$("#sync_data").addClass('ui-disabled');   
+				
+		// VISUALIZZA ROTELLA
+		$.mobile.loading('show');		
+	
+		DBSYNC.syncNow(callBackSyncProgress, function(result) {
 			
-	// VISUALIZZA ROTELLA
-	$.mobile.loading('show');		
-
-	DBSYNC.syncNow(callBackSyncProgress, function(result) {
-		
-     if (result.syncOK === true && result.serverAnswer.message != "undefined") {
-    
-    	 // NASCONDI ROTELLA
-    	 $.mobile.loading('hide');		
-
-     	 // LOGGA LA RISPOSTA DEL SERVER
-     	 console.log(result.serverAnswer.message);
-     	 
-     	 //	MOSTRA LA RISPOSTA DEL SERVER 
-         alert(result.serverAnswer.message);
-         
-         // PULISCI IL DATABASE
-         check_data_to_sync(db);
-
-     }
-     
-     else{
-     	alert("problema durante la sincronizzazione");
-     }
-     
-	});
+	     if (result.syncOK === true && result.serverAnswer.message != "undefined") {
+	    
+	    	 // NASCONDI ROTELLA
+	    	 $.mobile.loading('hide');		
+	
+	     	 // LOGGA LA RISPOSTA DEL SERVER
+	     	 console.log(result.serverAnswer.message);
+	     	 
+	     	 //	MOSTRA LA RISPOSTA DEL SERVER 
+	         alert(result.serverAnswer.message);
+	         
+	         // PULISCI IL DATABASE
+	         check_data_to_sync(db);
+	
+	     }
+	     
+	     else{
+	     	alert("problema durante la sincronizzazione");
+	     	
+ 			// ATTIVA PULSANTE
+     		$("#sync_data").removeClass('ui-disabled');   
+	     	
+ 			// NASCONDI ROTELLA
+			$.mobile.loading('hide');
+			
+			return;		
+	     }
+	     
+		});
+	}
 }
 
 function callBackSyncProgress(){
